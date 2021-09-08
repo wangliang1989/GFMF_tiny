6288ae086513cc5e35114e1dc060686678463f5d
王亮 <wangliang0222@gmail.com>
2021-09-08 11:31:43 +0800

    精简绘图脚本

--- draw.pl
+++ draw.pl
@@ -1,15 +1,12 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
-use List::Util qw(min max);
-use Time::Local;
 
-die "cali_11_10.txt not exist!" unless -e "cali_11_10.txt";
-my @wangliang = getdata("wangliang_cali_11_10.txt");
 my @you = getdata("cali_11_10.txt");
+my @wangliang = getdata("wangliang_cali_11_10.txt");
 
 system "gmt begin histgram";
-draw("-BWStr+t'Wangliang result' -Gred", @wangliang);
+draw("-BWStr+t'My result' -Gred", @wangliang);
 draw("-BWStr+t'Your result' -Gblue -Y10c", @you);
 system "gmt end show";
 
@@ -18,19 +15,18 @@ sub getdata{
     my @out;
     open (IN, "< $in") or die "cannot open $in";
     foreach (<IN>) {
-        #2018-08-15T00:28:20.86
         my ($origin) = split m/\s+/;
         my ($time) = (split "T", $origin)[1];
         my ($hour, $min, $sec) = split ":", $time;
-        $sec = (($hour * 60) + $min) * 60 + $sec;
-        push @out, $sec;
+        push @out, ($hour * 60 + $min) * 60 + $sec;
     }
     close(IN);
     return @out;
 }
 sub draw {
     my $cmd = shift;
-    $cmd = "-JX15c/7c -R0/86400/0/80 " . "-Bxa7200f3600+l'Time (s)' -Byaf $cmd";
+    $cmd = $cmd. " -JX15c/7c -R0/86400/0/80".
+                 " -Bxa7200f3600+l'Time (s)' -Byaf+l'No. of detections'";
     open (GMT, "| gmt histogram -D -W1p -T3600 $cmd") or die;
     print GMT "$_\n" foreach (@_);
     close(GMT);
